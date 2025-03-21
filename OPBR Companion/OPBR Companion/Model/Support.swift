90c81744b7aa6b85b651b80804b33cb65eb15553
Abdul Moiz <abdul.m01z10123@gmail.com>
2025-03-21 18:31:07 +0000

    Add Config.swift to .gitignore

---
+++ OPBR Companion/OPBR Companion/Model/Support.swift
@@ -0,0 +1,8 @@
+//
+//  Support.swift
+//  OPBR Companion
+//
+//  Created by Abdul Moiz on 21/03/2025.
+//
+
+import Foundation
---
+++ OPBR Companion/OPBR Companion/Supabase/Config.swift
@@ -0,0 +1,8 @@
+//
+//  Config.swift
+//  OPBR Companion
+//
+//  Created by Abdul Moiz on 21/03/2025.
+//
+
+import Foundation
